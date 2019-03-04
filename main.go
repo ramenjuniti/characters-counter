1681906b9403b604161ca51191bae0893b328ba5
ramenjuniti <ramenjuniti@gmail.com>
2019-03-05 00:07:59 +0900

    :tada: フォームに入力された文字をカウントできるようになった

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+	"unicode/utf8"
+)
+
+func count() {
+	document := js.Global().Get("document")
+
+	document.Call("getElementById", "countForm").Call("addEventListener", "submit", js.NewEventCallback(js.PreventDefault, func(e js.Value) {
+		charCount := utf8.RuneCountInString(document.Call("getElementById", "textArea").Get("value").String())
+		fmt.Println(charCount)
+		document.Call("getElementById", "result").Set("textContent", charCount)
+	}))
+}
 
 func main() {
-	fmt.Println("hello")
+	c := make(chan struct{}, 0)
+	count()
+	<-c
 }
